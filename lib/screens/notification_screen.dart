2bf23b75940efcc7f9c9f9d1eb514a72eb3deed7
Celesse Aisle Nacpil <notaisle@gmail.com>
2026-01-06 22:49:53 +0800

    feat: Initial setup for Lab Activity 4 - added CustomInformation widget - replaced loop with static list - fixed Expanded widget overflow

--- lib/screens/notification_screen.dart
+++ lib/screens/notification_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../widgets/notification.dart' as notif;
+import '../widgets/custom_info.dart' as notif;
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class NotificationScreen extends StatefulWidget {
@@ -9,36 +9,49 @@ class NotificationScreen extends StatefulWidget {
   State<NotificationScreen> createState() => _NotificationScreenState();
 }
 
-// General Enhancement: Add 10-20 Notifications with dummy data
 class _NotificationScreenState extends State<NotificationScreen> {
-  final List<Map<String, String>> notifications = List.generate(
-    10,
-    (index) => {
-      'name': 'User ${index + 1}',
-      'post': 'Title ${index + 1}',
-      'description': 'Description',
-      'time': '${index + 1}h ago'
-    },
-  );
-
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
       width: ScreenUtil().screenWidth,
-      child: ListView.separated(
-        padding: EdgeInsets.symmetric(vertical: 10.h), 
-        itemCount: notifications.length,
-        separatorBuilder: (context, index) => const Divider(),
-        itemBuilder: (context, index) {
-          final notifData = notifications[index];
-          return notif.Notification(
-            name: notifData['name']!,
-            post: notifData['post']!,
-            description: notifData['description']!,
-            time: notifData['time']!
-          );
-        },
+      child: ListView(
+        padding: EdgeInsets.symmetric(vertical: 10.h),
+        children: [
+          notif.CustomInformation(
+            name: 'Glyza Bermudez',
+            post: 'Posted a new comment',
+            description: '@Hajj Macaraig sige isa pang asar may kalalagyan ka talaga...',
+            time: '2h ago',
+            date: 'Jan 6, 2026',
+            numOfLikes: 5,
+            profileImageUrl: 'assets/images/gly_dp.jpg',
+          ),
+          const Divider(),
+
+          notif.CustomInformation(
+            name: 'Marygail Angelique',
+            post: 'Updated her profile picture',
+            description: '22 on 22',
+            time: '2w ago',
+            date: 'Dec 22, 2025',
+            numOfLikes: 150,
+            profileImageUrl: 'assets/images/marigold.jpg',
+          ),
+          const Divider(),
+
+          notif.CustomInformation(
+            name: 'Christopher Noval',
+            post: 'Gaming',
+            description: 'Who wants to play later?',
+            time: '1d ago',
+            date: 'Jan 04, 2026',
+            numOfLikes: 12,
+            profileImageUrl: 'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg',
+          ),
+          
+          
+        ],
       ),
     );
   }
---
+++ lib/widgets/custom_info.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import '../widgets/custom_font.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../screens/detail_screen.dart';
+
+class CustomInformation extends StatelessWidget {
+  const CustomInformation({
+    super.key,
+    required this.name,
+    required this.post,
+    required this.description,
+    required this.time,
+    this.icon = const Icon(Icons.person),
+    this.profileImageUrl = '',
+    this.atProfile = false,
+    required this.date,
+    this.imageUrl = '',
+    required this.numOfLikes
+  });
+
+  final String name;
+  final String post;
+  final String description;
+  final String time;
+  final Icon icon;
+  final String profileImageUrl;
+  final String date;
+  final int numOfLikes;
+  final String imageUrl;
+  final bool atProfile;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(ScreenUtil().setSp(15)),
+      child: InkWell(
+        onTap: () {
+          (atProfile)
+            ? print ('')
+            : Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DetailScreen(
+                  userName: name,
+                  postContent: description,
+                  date: date,
+                  numOfLikes: numOfLikes,
+                  imageUrl: imageUrl,
+                  profileImageUrl: profileImageUrl,
+                ),
+              ),
+            );
+        },  
+        child: Row(
+          children: [
+            (profileImageUrl == '')
+              ? icon
+              : CircleAvatar(
+                radius: ScreenUtil().setSp(27),
+                backgroundImage: (profileImageUrl == '') 
+                  ? null 
+                  : AssetImage(profileImageUrl),
+              ),
+            SizedBox(
+              width: ScreenUtil().setWidth(10),
+            ),
+
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CustomFont(
+                    text: name,
+                    fontSize: ScreenUtil().setSp(20),
+                    color: Colors.black,
+                    fontWeight: FontWeight.w800,
+                  ),
+                  CustomFont(
+                    text: post,
+                    fontSize: ScreenUtil().setSp(13),
+                    color: Colors.black,
+                  ),
+                  CustomFont(
+                    text: description,
+                    fontSize: ScreenUtil().setSp(12),
+                    color: Colors.black,
+                    fontStyle: FontStyle.italic,
+                  ),
+                  SizedBox(
+                    height: ScreenUtil().setHeight(5),
+                  ),
+                  CustomFont(
+                    text: date,
+                    fontSize: ScreenUtil().setSp(12),
+                    color: Colors.grey.shade400,
+                  ),
+                ],
+              ),
+            ),
+
+            // const Spacer(),
+            const Icon(Icons.more_horiz),
+          ],
+        ),
+      ),
+    );
+  }
+}
